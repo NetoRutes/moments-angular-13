9b4c6918471c8de72bc3bd9c168e1e892df36dd4
Wellington Dyon Fuck Rutes <netorutes@gmail.com>
2022-03-27 21:49:48 -0300

    data binding.

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponentComponent implements OnInit {
 
+  nome: string  = 'asd';
+
   constructor() { }
 
   ngOnInit(): void {
