8aaa6bc091b164b6b293f7b3e7ee593969dd4a7d
Wellington Dyon Fuck Rutes <netorutes@gmail.com>
2022-03-29 21:16:50 -0300

    Input angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'moments-angular-13';
+  title = "moments-angular-13";
+
+  userData = {
+    email: "netorutes@gmail.com",
+    birth: "12/02/1991",
+  };
+
+  userName = "Wellington";
 }
